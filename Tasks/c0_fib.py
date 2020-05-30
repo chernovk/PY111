256125ad4fd3a7f6851b5cda6058d9933372880a
Chernov <Chernov-k@yandex.ru>
2020-05-30 07:59:05 +0300

    fib recursive done

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,12 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
+
 
 
 def fib_iterative(n: int) -> int:
