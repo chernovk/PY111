4810183c17651fed8739212aa4bda1b1b714dc21
KonstantinChernov <Chernov-k@yandex.ru>
2020-06-16 21:13:59 +0300

    g0 done

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,12 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    while True:
+        sorting = False
+        for i in range(len(container) - 1):
+            if container[i] > container[i + 1]:
+                sorting = True
+                container[i], container[i + 1] = container[i + 1], container[i]
+        if not sorting:
+            break
     return container
