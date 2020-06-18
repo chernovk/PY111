bdfb54900bc97a7fbc239151c0d007abeb4502e4
KonstantinChernov <Chernov-k@yandex.ru>
2020-06-18 18:22:13 +0300

    g0 add reverse

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -1,17 +1,21 @@
 from typing import List
+import operator
 
 
-def sort(container: List[int]) -> List[int]:
+def sort(container: List[int], reverse=False) -> List[int]:
     """
     Sort input container with bubble sort
 
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    dc = {True: operator.lt,
+          False: operator.gt}
+    compare = dc[reverse]
     while True:
         sorting = False
         for i in range(len(container) - 1):
-            if container[i] > container[i + 1]:
+            if compare(container[i], container[i + 1]):
                 sorting = True
                 container[i], container[i + 1] = container[i + 1], container[i]
         if not sorting:
